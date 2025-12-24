38f403d76a3db07652d8e13891aca07c7901f665
hafsaa22 <hafsahounaoui@gmail.com>
2025-12-24 18:39:07 +0100

    Update Message.java

--- src/distributed/Message.java
+++ src/distributed/Message.java
@@ -3,12 +3,16 @@ package distributed;
 public class Message {
     private int senderId;
     private String payload;
+    private int lamportTimestamp; 
+    private int[] vectorClock;   
 
-    public Message(int senderId, String payload) {
+    public Message(int senderId, String payload, int lamportTimestamp, int[] vectorClock) {
         this.senderId = senderId;
         this.payload = payload;
+        this.lamportTimestamp = lamportTimestamp;
+        this.vectorClock = (vectorClock != null) ? Arrays.copyOf(vectorClock, vectorClock.length) : null;
     }
-
+    
     public int getSenderId() {
         return senderId;
     }
@@ -17,8 +21,21 @@ public class Message {
         return payload;
     }
 
+    public int getLamportTimestamp() {
+        return lamportTimestamp;
+    }
+
+    public int[] getVectorClock() {
+        return vectorClock;
+    }
+
     @Override
-    public String toString() {
-        return "Message{senderId=" + senderId + ", payload='" + payload + "'}";
+public String toString() {
+        return "Message{" +
+                "senderId=" + senderId +
+                ", payload='" + payload + '\'' +
+                ", lamport=" + lamportTimestamp +
+                ", vectorClock=" + Arrays.toString(vectorClock) +
+                '}';
     }
 }
