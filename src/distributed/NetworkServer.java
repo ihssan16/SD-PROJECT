4fb1e4eb1b8328d5adaec6a80bbc95334c162e2d
hafsaa22 <hafsahounaoui@gmail.com>
2025-12-24 18:57:15 +0100

    Update NetworkServer.java

--- src/distributed/NetworkServer.java
+++ src/distributed/NetworkServer.java
@@ -7,76 +7,51 @@ import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 
 public class NetworkServer extends Thread {
-
     private int port;
-    private int nodeId; 
+    private int nodeId;
     private BlockingQueue<Message> messageQueue;
+    private LamportClock lamport; 
+    private VectorClock vc;      
 
-    public NetworkServer(int port) {
-        this.port = port;
-    }
-
-    public NetworkServer(int port, int nodeId, BlockingQueue<Message> messageQueue) {
+    public NetworkServer(int port, int nodeId, BlockingQueue<Message> messageQueue, LamportClock lamport, VectorClock vc) {
         this.port = port;
         this.nodeId = nodeId;
         this.messageQueue = messageQueue;
+        this.lamport = lamport;
+        this.vc = vc;
     }
 
-    // public void start() {
-    //     try {
-    //         ServerSocket serverSocket = new ServerSocket(port);
-    //         System.out.println("[SERVER] Listening on port " + port);
-
-    //         while (true) {
-    //             Socket clientSocket = serverSocket.accept();
-    //             BufferedReader in = new BufferedReader(
-    //                     new InputStreamReader(clientSocket.getInputStream())
-    //             );
-
-    //             String message = in.readLine();
-    //             System.out.println("[RECEIVED] " + message);
-
-    //             clientSocket.close();
-    //         }
-
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
-     public void run() {
-        start();
-    }
-
-    public void start() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("[SERVER-" + nodeId + "] Listening on port " + port);  // CHANGED: Added nodeId
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("[SERVER-" + nodeId + "] Listening on port " + port);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream())
-                );
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String rawData = in.readLine();
 
-                String message = in.readLine();
-                
-                String[] parts = message.split(":", 2);
-                int senderId = Integer.parseInt(parts[0]);
-                String payload = parts.length > 1 ? parts[1] : "";
-                
-                Message msg = new Message(senderId, payload);
-                
-                messageQueue.put(msg);
-                
-                System.out.println("[SERVER-" + nodeId + "] Received and queued: " + msg);
+                if (rawData != null) {
+                    String[] parts = rawData.split("\\|", 4);
+                    int senderId = Integer.parseInt(parts[0]);
+                    int remoteLamport = Integer.parseInt(parts[1]);
+                    
+                    String vcStr = parts[2].replace("[", "").replace("]", "");
+                    int[] remoteVC = Arrays.stream(vcStr.split(",")).mapToInt(Integer::parseInt).toArray();
+                    String payload = parts[3];
+                    
+                    lamport.update(remoteLamport);
+                    vc.update(remoteVC);
 
+                    // Création du message enrichi avec les horloges reçues
+                    Message msg = new Message(senderId, payload, remoteLamport, remoteVC);
+                    messageQueue.put(msg);
+
+                    System.out.println("[SERVER-" + nodeId + "] Received: " + msg);
+                }
                 clientSocket.close();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
