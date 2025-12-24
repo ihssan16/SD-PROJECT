784ab799af5ac0ad8a37e81b4abd30532082a0e3
hafsaa22 <hafsahounaoui@gmail.com>
2025-12-24 19:05:12 +0100

    Update Main.java

--- src/distributed/Main.java
+++ src/distributed/Main.java
@@ -1,36 +1,39 @@
 package distributed;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        int totalNodes = 2; // Pour le vecteur d'horloge
+
+
+        // Node B (serveur)
 
         Node nodeB = new Node(2, "localhost", 5001);
-        BlockingQueue<Message> queueB = new LinkedBlockingQueue<>();
-        
-        LamportClock lamportB = new LamportClock();
-        VectorClock vcB = new VectorClock(totalNodes, nodeB.getId());
 
-        NetworkServer serverB = new NetworkServer(nodeB.getPort(), nodeB.getId(), queueB, lamportB, vcB);
-        serverB.start(); 
 
-        new ProcessorThread(nodeB.getId(), queueB).start();
+        // Lancer le serveur du Node B
 
-        try { 
-            Thread.sleep(1000); 
+        NetworkServer server = new NetworkServer(nodeB.getPort());
+
+        new Thread(() -> server.start()).start();
+
+
+        // Pause pour laisser le serveur démarrer
+
+        try {
+
+            Thread.sleep(1000);
+
         } catch (InterruptedException e) {}
 
+
+        // Node A envoie un message à Node B
+
         Node nodeA = new Node(1, "localhost", 5000);
-        
-        LamportClock lamportA = new LamportClock();
-        VectorClock vcA = new VectorClock(totalNodes, nodeA.getId());
 
-        System.out.println("Node A envoie un message avec horloges...");
+        NetworkClient.sendTo(nodeB, "Hello from Node A");
 
-        // Node A envoie un message à Node B en passant ses horloges
-        NetworkClient.sendTo(nodeB, nodeA.getId(), "Hello with clocks!", lamportA, vcA);
     }
-}
+
+} 
